bd9aa0b75ad0108076eec425e2fb604adb748b3c
Bill <sunyinqi0508@gmail.com>
2023-03-18 07:39:51 +0800

    Initial Commit

---
+++ MusicPlayer Watch App/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer Watch App
+//
+//  Created by BillSun on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
